c45e4d047dcf651068cc115714e1788c90972b3d
riya p <69091243+riyapragada@users.noreply.github.com>
2020-08-29 19:37:59 -0400

    Update BocciaPt2Pt3.py

--- BocciaPt2Pt3.py
+++ BocciaPt2Pt3.py
@@ -18,24 +18,27 @@ ev3 = EV3Brick()
 # Write your program here.
 ev3.speaker.beep(500)
 
+#Defining DriveBase
 left_motor = Motor(Port.C)
 right_motor = Motor(Port.B)
 wheel_diameter = 56
 axle_track = 115
-
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-
+#Defining Large Motor
 Large_Motor = Motor(Port.D)
 
-
+#From base to Frame.
 robot.straight(300)
 robot.turn(68)
 robot.straight(930)
+#Drop cubes into the Frame.
 Large_Motor.run_target(100,100,then = Stop.HOLD, wait = True)
 
 Large_Motor.run_angle(-100,100,then = Stop.HOLD, wait = True)
+#Back up to the Share part of the mission. 
 robot.straight(-450)
 robot.turn(-70)
+#Pushes the cube(s) into the other team's field and returns to base. 
 Large_Motor.run_target(100,100,then = Stop.HOLD, wait = True)
 robot.straight(220)
 Large_Motor.run_angle(-100,100,then = Stop.HOLD, wait = True)
